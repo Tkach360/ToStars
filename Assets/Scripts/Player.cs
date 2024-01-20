713ca585c6f34d3a228afb089f6902e20c54560f
Ткаченко Алексей <tkacenk002@mail.ru>
2024-01-20 12:56:40 +0700

    сделал бонусы

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 
 public class Player : MonoBehaviour
